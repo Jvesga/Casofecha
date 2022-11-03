6fee398baa0a01ed61f5327983e0257c01f6f46b
Jorge Alejandro Vesga Quintero <102545608+Jvesga@users.noreply.github.com>
2022-11-03 07:00:00 -0500

    ...

---
+++ ejecutable/main.java
@@ -0,0 +1,21 @@
+package ejecutable;
+
+import modelo.Fecha;
+
+public class main 
+{
+  public static void main(String[] args) 
+  {
+    Fecha f = new Fecha();
+    f.setDia(2);
+    f.setMes(10);
+    f.setAnio(1970);
+
+    System.out.println("Dia =" + f.getDia());
+    System.out.println("Mes =" + f.getMes());
+    System.out.println("Año =" + f.getAnio());
+    System.out.println(f);
+
+  }  
+
+}
---
+++ modelo/Fecha.java
@@ -0,0 +1,32 @@
+package modelo;
+
+public class Fecha
+{
+ private int dia;
+ private int mes;
+ private int anio;
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    public int getDia() {
+        return dia;
+    }
+    public int getMes() {
+        return mes;
+    }
+    public int getAnio() {
+        return anio;
+    }
+
+    //Metodo sobreescrito de la clase object
+    public String toString() {
+        return dia + "/" + mes + "/" + anio;
+    }
+} 
